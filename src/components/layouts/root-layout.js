dba1d49069138279c4b56ea58596fa6e4358749f
Uthaeus <romanlavery@gmail.com>
2023-05-30 13:57:24 -0600

    creted root layout and homepage components

---
+++ src/components/layouts/root-layout.js
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router";
+
+function RootLayout() {
+
+    return (
+        <div className="root-layout-container">
+
+            <Outlet />
+        </div>
+    );
+}
+
+export default RootLayout;

f2b9ee88deee92936b917929d909b7c6e323f4e6
Uthaeus <romanlavery@gmail.com>
2023-05-30 14:24:28 -0600

    added structure and styling to main nav

--- src/components/navigation/main-navigation.js
+++ src/components/navigation/main-navigation.js
@@ -2,18 +2,25 @@ import { NavLink, Link } from "react-router-dom";
 
 function MainNavigation() {
 
+    function logoutHandler() {
+        console.log('logoutHandler');
+    }
+
     return (
-        <div>
-            <div>
+        <div className="main-navigation-container">
+            <div className="main-navigation-header-wrapper">
                 logo/header
             </div>
 
-            <div>
+            <div className="main-navigation-links-wrapper">
                 <NavLink to="/" end className={({isActive}) => isActive ? 'main-nav-link link-active' : 'main-nav-link'}>Home</NavLink>
             </div>
 
-            <div>
-                auth links
+            <div className="main-navigation-auth-wrapper">
+                <NavLink to="/login" className="main-nav-link">Login</NavLink>
+                <NavLink to="/register" className="main-nav-link">Register</NavLink>
+
+                <Link onClick={logoutHandler} className="main-nav-link">Logout</Link>
             </div>
         </div>
     );

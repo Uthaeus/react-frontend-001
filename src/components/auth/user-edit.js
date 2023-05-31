ec94b18a2b4d1dc862b5b1ade3018294923157b3
Uthaeus <romanlavery@gmail.com>
2023-05-31 12:11:57 -0600

    created user edit form, created slide in animation on user detail page

---
+++ src/components/auth/user-edit.js
@@ -0,0 +1,64 @@
+import { useForm } from "react-hook-form";
+
+function UserEdit({ user }) {
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: user 
+    });
+
+    function buildForm(data) {
+        let formData = new FormData();
+
+        formData.append("user[username]", data.username);
+        formData.append("user[email]", data.email);
+        formData.append("user[password]", data.password);
+        formData.append("user[password_confirmation]", data.password_confirmation);
+
+        if (data.avatar[0]) {
+            formData.append("user[avatar]", data.avatar[0]);
+        }
+    }
+
+    const onSubmit = (data) => {
+        console.log(data);
+        let formData = buildForm(data);
+    };
+
+    return (
+        <div className="user-edit-container">
+            <form onSubmit={handleSubmit(onSubmit)} className="user-edit-form">
+                <div className="form-group mb-2">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" {...register("username", { required: true })} className="form-control" />
+                    {errors?.username && <span className="error-message">This field is required</span>}
+                </div>
+
+                <div className="form-group mb-2">
+                    <label htmlFor="email">Email</label>
+                    <input type="text" {...register("email", { required: true })} className="form-control" />
+                    {errors?.email && <span className="error-message">This field is required</span>}
+                </div>
+
+                <div className="form-group mb-2">
+                    <label htmlFor="avatar">Avatar</label>
+                    <input type="file" {...register("avatar")} className="form-control" />
+                </div>
+
+                <div className="form-group mb-2">
+                    <label htmlFor="password">Password</label>
+                    <input type="text" {...register("password", { required: true })} className="form-control" />
+                    {errors?.password && <span className="error-message">This field is required</span>}
+                </div>
+
+                <div className="form-group mb-3">
+                    <label htmlFor="password_confirmation">Confirm Password</label>
+                    <input type="text" {...register("password_confirmation", { required: true })} className="form-control" />
+                    {errors?.password_confirmation && <span className="error-message">This field is required</span>}
+                </div>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    );
+}
+
+export default UserEdit;    
--- src/pages/user-page.js
+++ src/pages/user-page.js
@@ -1,9 +1,40 @@
+import { useContext } from "react";
+
+import image from '../assets/images/placeholder-sm.png';
+
+import { UserContext } from "../store/user-context";
+import UserEdit from "../components/auth/user-edit";
 
 function UserPage() {
+    const { user } = useContext(UserContext);
+
+    let avatar = user.avatar ? `http://localhost:4000${user.avatar.url}` : image;
+
+    function showFormHandler() {
+        let element = document.querySelector('.user-edit-container');
+        element.classList.toggle('user-edit-slide');
+    }
 
     return (
-        <div>
-            <h1>User Page</h1>
+        <div className="user-page-container">
+            <div className="user-wrapper">
+                <div className="user-info-wrapper">
+                    <h1 className="user-info-username">{user.username}</h1>
+                    <h2 className="user-info-email">{user.email}</h2>
+                    <p className="user-info-item">user created: {user.created_at.split('T')[0]}</p>
+                    <p className="user-info-item">user updated: {user.updated_at.split('T')[0]}</p>
+                    <p className="user-info-item">comments: {user.comments?.length}</p>
+                    <p className="user-info-item">posts: {user.posts?.length}</p>
+
+                    <button onClick={showFormHandler} className="user-info-edit-btn">Edit</button>
+                </div>
+
+                <div className="user-avatar-wrapper">
+                    <img src={avatar} alt="user avatar" width='50%' />
+                </div>
+            </div>
+
+            <UserEdit user={user} />
         </div>
     );
 }

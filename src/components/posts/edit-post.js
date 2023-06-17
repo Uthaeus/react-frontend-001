ced1ed9178bf57db8f918aa3bbb019c953f481d2
Uthaeus <romanlavery@gmail.com>
2023-06-17 15:41:12 -0600

    added styling to edit post

--- src/components/posts/edit-post.js
+++ src/components/posts/edit-post.js
@@ -1,12 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { useParams, Link} from "react-router-dom";
 
+import { UserContext } from "../../store/user-context";
 
 import PostForm from "./post-form";
 
 function EditPost() {
     const { id } = useParams();
     const [post, setPost] = useState(null);
+    const { user } = useContext(UserContext);
 
     useEffect(() => {
         fetch(`http://localhost:4000/posts/${id}`)
@@ -26,9 +28,13 @@ function EditPost() {
         <div className="edit-post-container">
             <h1 className="edit-post-title">Edit Post</h1>
 
-            {post && <PostForm post={post} />}
+            {post && <PostForm post={post} user={user} />}
 
-            <Link to="/posts">Back to Posts</Link>
+            <div className="edit-post-actions">
+                <Link className="edit-post-action back-link" to="/posts">Back to Posts</Link>
+                <Link className="edit-post-action post-link" to={`/posts/${id}`}>View Post</Link>
+                <Link className="edit-post-action delete-link" to={`/posts/${id}/delete`}>Delete Post</Link>
+            </div>
         </div>
     );
 }
